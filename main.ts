99e84b280a6f86077bbe3b6f7cc85ad051d709e2
mama omo <team@omo.earth>
2020-10-17 15:02:04 +0200

    ficxed env

--- main.ts
+++ main.ts
@@ -1,15 +1,17 @@
 import App from "./app/App.svelte";
-import { CirclesWrapper } from "./kernel/circles-protocol/circlesActions";
+// import { CirclesWrapper } from "./kernel/circles-protocol/circlesActions";
 declare global {
     interface Window {
     }
 }
-
+console.log("HA=",process.env.HUB_ADDRESS)
+console.log("env",JSON.stringify(process.env));
+window["env"] = process.env;
 export const isLocal = window.location.hostname == "localhost"
     || window.location.hostname == "127.0.0.1"
     || window.location.hostname == "omo.local";
 
-console.log(CirclesWrapper.safeOwnerFromKeyphrase(""))
+// console.log(CirclesWrapper.safeOwnerFromKeyphrase(""))
 
 // export async function xfetch(hash:string, page?:string) {
 //   let baseUrl = isLocal
